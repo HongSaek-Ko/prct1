036b0e161b10883a70e1951a5417a5bcb82300e1
HongSaek-Ko <bh1152@naver.com>
2025-08-12 15:20:06 +0900

    first commit

---
+++ src/main/java/com/toypj1/prct1/Prct1Application.java
@@ -0,0 +1,13 @@
+package com.toypj1.prct1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prct1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prct1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/toypj1/prct1/Prct1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toypj1.prct1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prct1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
